290d12fd96f6ec02130573d6ff8848f00af26919
Jay1Lyg <1559011067@qq.com>
2019-01-28 18:52:18 +0800

    整理2

--- qiaojie/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  qiaojie
-//
-//  Created by will-design-1 on 2018/10/23.
-//  Copyright © 2018年 will-design-1. All rights reserved.
-//
-
-import Foundation
--- qiaojie/header.m
+++
@@ -1,9 +0,0 @@
-//
-//  header.m
-//  qiaojie
-//
-//  Created by will-design-1 on 2018/9/17.
-//  Copyright © 2018年 will-design-1. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
